8af421993744e13962516352582b2866c8aeaeff
William Jung & Alex S & Robert Washington & Dumitru Gutu <pair+William-Jung.alexstevenson19.amorobert.dumitrugutu@devbootcamp.com>
2016-07-20 15:50:52 -0500

    Add poly to votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
   belongs_to :user
+  belongs_to :votable, polymorphic: true
 
   def cast(form_vote)
     vote = form_vote.to_i
