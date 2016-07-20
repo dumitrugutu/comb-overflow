3045f889bed170367e7a2546a288ab5918b9904a
William Jung & Alex S & Robert Washington & Dumitru Gutu <pair+William-Jung.alexstevenson19.amorobert.dumitrugutu@devbootcamp.com>
2016-07-20 15:17:06 -0500

    Create associations for questions

--- db/migrate/20160720142421_create_answers.rb
+++ db/migrate/20160720142421_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :content, null: false
+      t.integer :answerer_id, null: false
       t.integer :question_id, null: false
 
       t.timestamps(null: false)
