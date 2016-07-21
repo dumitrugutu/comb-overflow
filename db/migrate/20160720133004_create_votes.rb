86e3f5873f5433ad6945c37bba44804216851db1
William Jung & Alex S & Robert Washington & Dumitru Gutu <pair+William-Jung.alexstevenson19.amorobert.dumitrugutu@devbootcamp.com>
2016-07-21 13:23:18 -0500

    Update

--- db/migrate/20160720133004_create_votes.rb
+++ db/migrate/20160720133004_create_votes.rb
@@ -3,7 +3,7 @@ class CreateVotes < ActiveRecord::Migration
     create_table :votes do |t|
       t.integer :point, default: 0
       t.integer :votable_id
-      t.integer :votable_type
+      t.string :votable_type
       t.integer :user_id, null: false
 
       t.timestamps(null: false)
