98bdf49df400dc0f901c6ac1c7f2b858d126792f
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-07-22 12:29:31 -0500

    Add a few more questions and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,24 @@ User.create(display_name: "Wobert Rashington", email: "wobert@dc.com", password:
 
 User.create(display_name: "Sleks Atevenson", email: "slacks@flax.nafta", password: "password")
 
+Question.create(content: "Does parting my hair to the left mean that I'm left-handed?", user_id: 1)
 Question.create(content: "How does Dumitru's hair not move all day? Does he wake up like this?", user_id: 2)
-Question.create(content: "Does Rogaine work the same way for facial hair?", user_id: 2)
+Question.create(content: "I want to comb Dracula's hair, but the Pope took my pitch fork. Where can I get another one?", user_id: 3)
+
+Question.create(content: "I bought some glue to wrap presents for christmas and I inadvertently sniffed some of it. Now my hair is all messed up. How do I save the puppy?", user_id: 1)
 Question.create(content: "What do I put in my hair to make it flow like the falls of Niagara", user_id: 2)
+Question.create(content: "What is love? Baby don't hurt me, don't hurt me, no more", user_id: 3)
+Question.create(content: "How can I sleep at night so I don't have to comb my hair in the morning?", user_id: 1)
+
+Question.create(content: "Does Rogaine work the same way for facial hair?", user_id: 2)
+Question.create(content: "My best friend is getting married and I need to buy a nice toupee to cover that barren top of my head. Any suggestions?", user_id: 1)
 Question.create(content: "If I spray painted my bald spot would people notice? Lol", user_id: 2)
 
 Answer.create(content: "Fk off buddy. Thx.", user_id: 1, question_id: 1)
+Answer.create(content: "Try using a toupee or the heat will damage your already barren head.", user_id: 1, question_id: 5)
+
 Answer.create(content: "He sleeps standing up.", user_id: 4, question_id: 1)
+Answer.create(content: "HAHA! It takes years of parctice. And a bucket of molding cream.", user_id: 1, question_id: 1)
 
 # 10.times do
 #   User.create(display_name: Faker::Name.name , email: Faker::Internet.email, password: "password")
