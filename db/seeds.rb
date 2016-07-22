8b14e3f0516d7adcd3dedca936e51bdae3c87f15
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-07-22 09:29:10 -0500

    Seed data with our names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,28 @@
-10.times do
-  User.create(display_name: Faker::Name.name , email: Faker::Internet.email, password: "password")
-end
+User.create(display_name: "Gumitru Dutu" , email: "dmt@hairstyle.com", password: "password")
 
-25.times do
-  Question.create(content: Faker::Hipster.sentence + "?", user: User.find(rand(1..10)))
-end
+User.create(display_name: "Jill Wung", email: "will@gmail.com", password: "password")
 
-25.times do
-  Answer.create(content: Faker::Hipster.sentence, user: User.find(rand(1..10)), question: Question.find(rand(1..25)))
-end
+User.create(display_name: "Wobert Rashington", email: "wobert@dc.com", password: "password")
 
-20.times do
-  Comment.create(content: Faker::Hipster.sentence, user: User.find(rand(1..10)), commentable_id: rand(1..25), commentable_type: "Question")
-end
+User.create(display_name: "Sleks Atevenson", email: "slacks@flax.nafta", password: "password")
 
-20.times do
-  Comment.create(content: Faker::Hipster.sentence, user: User.find(rand(1..10)), commentable_id: rand(1..25), commentable_type: "Answer")
-end
+
+# 10.times do
+#   User.create(display_name: Faker::Name.name , email: Faker::Internet.email, password: "password")
+# end
+
+# 25.times do
+#   Question.create(content: Faker::Hipster.sentence + "?", user: User.find(rand(1..10)))
+# end
+
+# 25.times do
+#   Answer.create(content: Faker::Hipster.sentence, user: User.find(rand(1..10)), question: Question.find(rand(1..25)))
+# end
+
+# 20.times do
+#   Comment.create(content: Faker::Hipster.sentence, user: User.find(rand(1..10)), commentable_id: rand(1..25), commentable_type: "Question")
+# end
+
+# 20.times do
+#   Comment.create(content: Faker::Hipster.sentence, user: User.find(rand(1..10)), commentable_id: rand(1..25), commentable_type: "Answer")
+# end
